27fb67b94ae1af9c0a59b15121f524c10ef9f84b
Michael Whalen <michael.whalen@sitechhs.com>
2020-03-22 18:26:15 -0400

    sdfilguh

---
+++ index.js
@@ -0,0 +1,32 @@
+/* let string = "string";
+const edis = "Edis";
+let sentence = "Hello " + "Vicky";
+let sentence2 = " and also Angel";
+//let combine = sentence + sentence2;
+let combine = `${sentence}${sentence2}`; //ES2015
+
+let student1 = "Sandra";
+let student2 = "Kelly";
+let courseName = "AP CSP";
+let teacherName = "Whalen"; */
+/* const longerSentence =
+  student1 +
+  " and " +
+  student2 +
+  " are amazing and will be taking " +
+  courseName +
+  " next year with " +
+  teacherName;
+
+console.log(longerSentence); */
+/* let longSentence = `${student1} and ${student2} will be taking ${courseName} with ${teacherName} next year`;
+console.log(longSentence);
+ */
+
+/////////////////////////////////
+//Numbers Integers
+/* let year = 2020;
+let nextYear = 2020 + 1;
+let past = year / 2;
+let lastYear = year - 1;
+let farFuture = year * 100; */
