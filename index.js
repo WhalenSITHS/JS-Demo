c195a926d8bab876f1b74d232a8217f8a442e907
Michael Whalen <michael.whalen@sitechhs.com>
2020-03-24 08:29:25 -0400

    dfgsdfgsdfgsdfg

--- index.js
+++ index.js
@@ -30,3 +30,47 @@ let nextYear = 2020 + 1;
 let past = year / 2;
 let lastYear = year - 1;
 let farFuture = year * 100; */
+//////////////////////////
+//Booleans, null, undefined
+/* let trueStatement = true;
+let falseStatement = false;
+
+let undefinedVar; */
+//console.log(undefinedVar); //do this a lot
+//console.log(nonExistent); //null never declared
+//console.log(truuStatement);
+
+///if/ else if statements
+//let student = "Karen";
+//let age = 17;
+// console.log(student === "Karen"); //true
+// console.log(student === "karen"); //false
+// console.log(student == "karen"); //false
+//console.log(age === "17"); //false
+//console.log(age == "17"); //true
+
+//let age = 16;
+//let name = "Kristy";
+/* console.log(age < 17);
+console.log(age <= 17);
+console.log(age >= 17); */
+//console.log(age == 16 || age == 17); // if age equals 16 OR age == 17 then true
+//console.log(age == 16 && name === "Angel");
+//console.log(age !== 1);
+
+///////If statements
+/* let age = 18;
+if (age < 18) {
+  console.log("You are a child");
+} else if (age >= 18) {
+  console.log("you are an adult now");
+}
+ */
+/* let school = "";
+if (school == "elementary") {
+  console.log("you are in elementary school");
+} else if (school == "middle") {
+  console.log("you are in middle school");
+} else {
+  console.log("you are in high school");
+} */
