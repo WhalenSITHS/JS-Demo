a8aed5b38d201c1c21849d669520ac137d4bb336
Michael Whalen <michael.whalen@sitechhs.com>
2020-03-25 10:53:04 -0400

    demo challenge

---
+++ app.js
@@ -0,0 +1,80 @@
+/* let string = "I am a string";
+let sentence = "Hello Angel " + "and Vicky";
+let combine = string + sentence; */
+/* 
+let student = "Sandra";
+let student2 = "Gia";
+let courseName = "AP CSP";
+let teacherName = "Whalen";
+let sentence =
+  student +
+  " and " +
+  student2 +
+  " are taking " +
+  courseName +
+  " with " +
+  teacherName;
+
+let betterSentence = `${student} and ${student2} are taking ${courseName} with ${teacherName} next year`; */
+
+////////////Numbers and Integers
+/* let year = 2020;
+let nextYear = year - 1;
+let past = 2020 / 2;
+let future = year * 2; */
+
+/////Booleans
+//let trueStatement = false;
+
+///////Null and undefined
+//let richard;
+
+/////Boolean Logic or if/else
+
+//let age = 18;
+//let baoSquared = "Bao Bao";
+//console.log(baoSquared === "bao Bao");
+//console.log(age == "17");
+/* 
+let age = 45;
+if (age < 18) {
+  console.log("You are a child");
+} else if (age == 18) {
+  console.log("Happy Birthday");
+} else {
+  console.log("You are over 18");
+}
+
+*/
+//red,green, yellow
+/* let light = "Edis";
+if (light === "green") {
+  console.log("Go");
+} else if (light === "Yellow") {
+  console.log("Caution!");
+} else {
+  console.log("Stop");
+}
+ */
+/* let kristyLactInt = false;
+let baoBaoLactint = false;
+
+if (kristyLactInt === true && baoBaoLactint == true) {
+  console.log("Get Almond Milk");
+} else if (kristyLactInt === true || baoBaoLactint == true) {
+  console.log("Get Both");
+} else {
+  console.log("Only milk");
+}
+ */
+
+let temp = 72;
+if (temp < 68) {
+  console.log("Too Cold");
+} else if (temp >= 68 && temp <= 72) {
+  console.log("Perfect");
+} else if (temp > 72 && temp <= 76) {
+  console.log("is warm");
+} else {
+  console.log("Too Hot");
+}
